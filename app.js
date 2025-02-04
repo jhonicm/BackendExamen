0907ba5adf10d113091c7fb3ce1552b7993f04ce
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2025-02-04 13:19:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const rutasProduccion = require('./routes/produccionRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://frontend-examen-plum.vercel.app',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/producciones', rutasProduccion);
 
